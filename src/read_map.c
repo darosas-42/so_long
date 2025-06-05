9f1f938bfd5a3465696ee4c76a151e2ff3afd7b4
Daniel Rosas Nunez <darosas-@c2r8s3.42malaga.com>
2025-06-05 18:14:45 +0200

    finished

--- src/read_map.c
+++ src/read_map.c
@@ -6,7 +6,7 @@
 /*   By: darosas- <darosas-@student.42malaga.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/28 17:10:21 by drosas-n          #+#    #+#             */
-/*   Updated: 2025/06/02 17:55:59 by darosas-         ###   ########.fr       */
+/*   Updated: 2025/06/05 18:09:16 by darosas-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,14 +27,14 @@ int	copy_map(int fd, t_game *game, char *line)
 	while (game->i < game->y)
 	{
 		game->x = ft_strlen(line);
-		game->map[game->i] = malloc(sizeof(char) * game->x + 1);
-		game->map_copy[game->i] = malloc(sizeof(char) * game->x + 1);
+		game->map[game->i] = malloc(sizeof(char) * (game->x + 1));
+		game->map_copy[game->i] = malloc(sizeof(char) * (game->x + 1));
 		if (!game->map[game->i] || !game->map_copy[game->i])
 			return (EXIT_FAILURE);
 		if (game->i == game->y - 1)
 		{
-			ft_strlcpy(game->map[game->i], line, game->x + 1);
-			ft_strlcpy(game->map_copy[game->i], line, game->x + 1);
+			ft_strlcpy(game->map[game->i], line, (game->x + 1));
+			ft_strlcpy(game->map_copy[game->i], line, (game->x + 1));
 		}
 		else
 		{
@@ -64,8 +64,8 @@ int	read_map(char **argv, t_game *game)
 	read_lines(game, fd, line);
 	close(fd);
 	fd = open(argv[1], O_RDONLY);
-	game->map = malloc(sizeof(char *) * game->y + 1);
-	game->map_copy = malloc(sizeof(char *) * game->y + 1);
+	game->map = malloc(sizeof(char *) * (game->y + 1));
+	game->map_copy = malloc(sizeof(char *) * (game->y + 1));
 	if (!game->map || !game->map_copy)
 		return (EXIT_FAILURE);
 	line = get_next_line(fd);
